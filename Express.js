aea77bae14636f4d8e31f318dfc7c47e316369d7
iamrackshana <rackshana.b2022csbs@sece.ac.in>
2024-03-08 12:23:07 +0530

    Changes get committed

--- Express.js
+++ Express.js
@@ -1,22 +1,43 @@
 const express = require('express');
+const mongoose = require ('mongoose');
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/expense-tracker');
+
+const expenseSchema = new mongoose.Schema({
+    description : String,
+    amount : Number
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
 
 app.use(express.json());
 app.use(express.static('public'));
 
-let expenses = [];
-
-app.get('/api/expenses', (req, res) => {
-  res.json(expenses);
+app.get('/api/expenses',async(req,res)=>{
+    const expenses = await Expense.find();
+    res.json(expenses);
 });
 
-app.post('/api/expenses', (req, res) => {
-  const newExpense = req.body;
-  expenses.push(newExpense);
-  res.status(201).json(newExpense);
+app.post('/api/expenses',async(req,res)=>{
+    const newExpense = new Expense(req.body);
+    await newExpense.save();
+    res.status(201).json(newExpense);
 });
 
+app.put('/api/expenses/:id',async(req, res)=>{
+    const {id}= req.params;
+    const updatedExpense = await Expense.findByIdAndUpdate(id, req.body,{new: true});
+    res.json(updatedExpense);
+});
+
+app.delete('/api/expenses/:id',async (req,res)=>{
+    const {id} = req.params;
+    await Expense.findByIdAndDelete(id);
+    res.sendStatus(204);
+  })
+  
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+    console.log(`Server running at http://localhost:${port}`);
+  });
